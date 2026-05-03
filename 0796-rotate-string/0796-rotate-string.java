3c3e65c77d8f130662fe59f44cf089f615807685
Sandeep Yadav <149508952+YadavSandeep19@users.noreply.github.com>
2026-05-03 23:12:39 +0530

    Time: 0 ms (100%), Space: 42.7 MB (58.69%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        // Length must match
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        // Core logic
+        String doubled = s + s;
+
+        return doubled.contains(goal);
+    }
+}
