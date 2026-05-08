67fb4ec2f9815c24ae62f01b506036b800ef6cca
Sandeep Yadav <149508952+YadavSandeep19@users.noreply.github.com>
2026-05-08 17:36:00 +0530

    Time: 1 ms (76.51%), Space: 46.3 MB (97.17%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,34 +1,36 @@
-// class Solution {
-//     public int removeDuplicates(int[] nums) {
-//         int i = 0;
-//         for (int j = 1; j < nums.length; j++) {
-//             if (nums[i] != nums[j]) {
-//                 int temp = nums[i + 1];
-//                 nums[i + 1] = nums[j];
-//                 nums[j] = temp;
-//                 i++;
-//             }
-//         }
-//         return i + 1;
-//     }
-// }
-
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int start= 0;
-        int temp= 1;
-        while(temp<nums.length){
-            if(nums[temp] == nums[temp - 1]){
-                temp++;
-                continue;
+        if(nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                int temp = nums[i + 1];
+                nums[i + 1] = nums[j];
+                nums[j] = temp;
+                i++;
             }
-           nums[start + 1] = nums[temp];
-            start++;
-            temp++;
         }
-        return start + 1;
+        return i + 1;
     }
 }
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         if(nums.length == 0) return 0;
+//         int start= 0;
+//         int temp= 1;
+//         while(temp<nums.length){
+//             if(nums[temp] == nums[temp - 1]){
+//                 temp++;
+//                 continue;
+//             }
+//            nums[start + 1] = nums[temp];
+//             start++;
+//             temp++;
+//         }
+//         return start + 1;
+//     }
+// }
 
 
 
