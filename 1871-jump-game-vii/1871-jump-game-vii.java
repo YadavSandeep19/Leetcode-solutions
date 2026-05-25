cb8459322c3877e59dd01b7cd808e0b79a97c58e
Sandeep Yadav <149508952+YadavSandeep19@users.noreply.github.com>
2026-05-25 22:51:57 +0530

    Time: 11 ms (51.15%), Space: 48 MB (48.31%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+
+        int count = 0; // number of reachable positions in current window
+
+        for (int i = 1; i < n; i++) {
+            if (i - minJump >= 0 && dp[i - minJump]) {
+                count++;
+            }
+            if (i - maxJump - 1 >= 0 && dp[i - maxJump - 1]) {
+                count--;
+            }
+
+            dp[i] = (s.charAt(i) == '0' && count > 0);
+        }
+
+        return dp[n - 1];
+    }
+}
