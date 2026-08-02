ee7a271e4346eb76f6eeae24a5ca7621e3cb106e
Sandeep Yadav <149508952+YadavSandeep19@users.noreply.github.com>
2026-08-02 21:18:49 +0530

    Time: 11 ms (7.25%), Space: 48.4 MB (7.93%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,30 @@
+// class Solution {
+//     public boolean stoneGame(int[] piles) {
+//         return true;
+//     }
+// }
+
 class Solution {
+    private Integer[][] dp;
+
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        dp = new Integer[n][n];
+        return solve(piles, 0, n - 1) > 0;
+    }
+
+    private int solve(int[] piles, int left, int right) {
+        if (left == right) {
+            return piles[left];
+        }
+
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
+
+        int pickLeft = piles[left] - solve(piles, left + 1, right);
+        int pickRight = piles[right] - solve(piles, left, right - 1);
+
+        return dp[left][right] = Math.max(pickLeft, pickRight);
     }
 }
