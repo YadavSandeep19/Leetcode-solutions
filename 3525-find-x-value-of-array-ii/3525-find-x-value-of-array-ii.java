f2098563b85fa3f6b1509e32945dd74bda3b4ffd
Sandeep Yadav <149508952+YadavSandeep19@users.noreply.github.com>
2026-09-22 23:04:39 +0530

    Time: 303 ms (21.05%), Space: 284.5 MB (26.32%) - LeetHub

---
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.java
@@ -0,0 +1,128 @@
+class Solution {
+
+    class Node {
+        int prod;
+        int[] cnt;
+
+        Node(int k) {
+            cnt = new int[k];
+        }
+    }
+
+    int k;
+    Node[] tree;
+
+    public int[] resultArray(int[] nums, int k, int[][] queries) {
+
+        this.k = k;
+        int n = nums.length;
+
+        tree = new Node[4 * n];
+
+        build(1, 0, n - 1, nums);
+
+        int[] ans = new int[queries.length];
+
+        for (int q = 0; q < queries.length; q++) {
+
+            int index = queries[q][0];
+            int value = queries[q][1];
+            int start = queries[q][2];
+            int x = queries[q][3];
+
+            // Update
+            update(1, 0, n - 1, index, value % k);
+
+            // Query [start, n-1]
+            Node res = query(1, 0, n - 1, start, n - 1);
+
+            ans[q] = res.cnt[x];
+        }
+
+        return ans;
+    }
+
+    private void build(int node, int l, int r, int[] nums) {
+
+        tree[node] = new Node(k);
+
+        if (l == r) {
+            int val = nums[l] % k;
+
+            tree[node].prod = val;
+            tree[node].cnt[val] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, nums);
+        build(node * 2 + 1, mid + 1, r, nums);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private void update(int node, int l, int r, int index, int value) {
+
+        if (l == r) {
+            tree[node] = new Node(k);
+            tree[node].prod = value;
+            tree[node].cnt[value] = 1;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (index <= mid) {
+            update(node * 2, l, mid, index, value);
+        } else {
+            update(node * 2 + 1, mid + 1, r, index, value);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private Node query(int node, int l, int r, int ql, int qr) {
+
+        if (ql <= l && r <= qr) {
+            return tree[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid) {
+            return query(node * 2, l, mid, ql, qr);
+        }
+
+        if (ql > mid) {
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+
+    private Node merge(Node left, Node right) {
+
+        Node res = new Node(k);
+
+        // Product of complete segment
+        res.prod = (left.prod * right.prod) % k;
+
+        // Prefixes completely inside left
+        for (int r = 0; r < k; r++) {
+            res.cnt[r] += left.cnt[r];
+        }
+
+        // Prefixes that start in left and continue into right
+        for (int r = 0; r < k; r++) {
+            int newRemainder = (left.prod * r) % k;
+            res.cnt[newRemainder] += right.cnt[r];
+        }
+
+        return res;
+    }
+}
